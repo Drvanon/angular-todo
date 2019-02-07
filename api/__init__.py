92d3e2397e2952ae0732621623f166274ecf1701
Robin A. Dorstijn <robbiedorstijn@gmail.com>
2019-02-07 17:50:44 +0100

    Make api CORS

--- api/__init__.py
+++ api/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
@@ -13,6 +14,8 @@ def create_app(test_config=None):
         SQLALCHEMY_DATABASE_URI='sqlite:///{}'.format(os.path.join(app.instance_path, 'api.sqlite')),
         SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
+
+    CORS(app)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
